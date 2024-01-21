908608cee2624182422bb8465df40c1106b29da7
ilobosok <121606196+ivan-lab-pvls@users.noreply.github.com>
2024-01-21 15:15:04 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,19 @@ import 'onBoarding/OnBoardingScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final prefs = await SharedPreferences.getInstance();
-  final onboardingComplete = prefs.getBool('onboardingComplete') ?? false;
-
-  runApp(MyApp(onboardingComplete: onboardingComplete));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final bool onboardingComplete;
+ 
 
-  const MyApp({Key? key, required this.onboardingComplete}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: onboardingComplete ? MainGameScreen() : const OnBoardingScreen(),
+      home:  OnBoardingScreen(),
     );
   }
 }
